b1b04f0ee12b275e652ab70fa65c25a9ad3ffe80
1994-MR2-SW20 <71955406+1seco@users.noreply.github.com>
2024-11-24 12:17:35 +0800

    yay

--- C/rec.c
+++ C/rec.c
@@ -13,7 +13,7 @@ Node *create_btree(Node* root_node, unsigned node_num) {
     /* Taking any selected node, or NULL. Allocs mem for node_num */
     // do the linear method it seems to be much easier than the recursive thing
     Node *node_arr = NULL;
-    bool even = (node_num % 2) ? false : true;
+    char even = (node_num % 2) ? 0 : -1;
     unsigned off = 0;
     
     if (!root_node) {
@@ -30,13 +30,13 @@ Node *create_btree(Node* root_node, unsigned node_num) {
     root_node->l = node_arr;
     root_node->r = node_arr + 1;
     
-    for (; off < (node_num / 2) - 1; off++) {
+    for (; off < (node_num / 2) + even ; off++) {
         node_arr[off].l = node_arr + ((off + 1) * 2);
         printf("%p\n{\n\tl=%p &\n\tr=%p\n}\n", (void*)(node_arr + off), (void*)node_arr[off].l, (void*)node_arr[off].r);
     }
     
     off = 0;
-    for (; off < (node_num / 2) - 2; off++) {
+    for (; off < (node_num / 2) + even - 1; off++) {
         node_arr[off].r = node_arr + ((off + 1) * 2);
     }
 
@@ -69,8 +69,9 @@ int main(void) {
     Node root_node = {0, NULL, NULL};
     int trv_count;
     
-    if (!create_btree(&root_node, 6)) {
+    if (!create_btree(&root_node, 3)) {
         fputs("create_btree ret NULL\n", stderr);
+        free(root_node.l);
         exit(EXIT_FAILURE);
     }
     /* traverse_btree */
